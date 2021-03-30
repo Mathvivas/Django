f591556ea5eb55514b3276c92b727e48ebc1c1a4
Mathvivas <mathvivas@hotmail.com>
2021-03-30 15:53:39 -0300

    Migração dos dados para o Banco Postgre

--- Receitas/receitasApp/models.py
+++ Receitas/receitasApp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Receita(models.Model):        # Subclasse
+    nome_receita = models.CharField(max_length=200)     # Campo para nome
+    ingredientes = models.TextField()                   # Campo para texto grande
+    modo_preparo = models.TextField()
+    tempo_preparo = models.IntegerField
+    rendimento = models.CharField(max_length=100)
+    categoria = models.CharField(max_length=100)
+    data_receitas = models.DateTimeField(default=datetime.now, blank=True)
+
+    # Terminal, mesma pasta de manage.py:
+    ###     python manage.py makemigrations ###
+    ###     python manage.py migrate ###
