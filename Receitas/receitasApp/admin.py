0eaacf45fc9986b6460c4e4d956b86989a6aa632
Mathvivas <mathvivas@hotmail.com>
2021-03-30 16:32:55 -0300

    Configuração do Admin

--- Receitas/receitasApp/admin.py
+++ Receitas/receitasApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Receita
+
+# Terminal: python manage.py createsuperuser
 
 # Register your models here.
+admin.site.register(Receita)
--- Receitas/receitasApp/models.py
+++ Receitas/receitasApp/models.py
@@ -6,7 +6,7 @@ class Receita(models.Model):        # Subclasse
     nome_receita = models.CharField(max_length=200)     # Campo para nome
     ingredientes = models.TextField()                   # Campo para texto grande
     modo_preparo = models.TextField()
-    tempo_preparo = models.IntegerField
+    tempo_preparo = models.IntegerField()
     rendimento = models.CharField(max_length=100)
     categoria = models.CharField(max_length=100)
     data_receitas = models.DateTimeField(default=datetime.now, blank=True)
