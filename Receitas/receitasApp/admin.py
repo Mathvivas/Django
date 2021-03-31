71b6742f0865eb95d172e73dfcdad6c38178cd87
Mathvivas <mathvivas@hotmail.com>
2021-03-30 22:24:10 -0300

    Melhorias no Admin e Criação de uma Nova App

--- Receitas/receitasApp/admin.py
+++ Receitas/receitasApp/admin.py
@@ -3,5 +3,13 @@ from .models import Receita
 
 # Terminal: python manage.py createsuperuser
 
-# Register your models here.
-admin.site.register(Receita)
+class ListandoReceitas(admin.ModelAdmin):
+    list_display = ('id', 'nome_receita', 'categoria')     # Mostra a lista de receitas no admin
+    list_display_link = ('id', 'nome_receita')
+    search_fields = ('nome_receita',)       # Precisa de uma vírgula, pois necessita de uma lista ou tupla
+    list_filter = ('categoria',)
+    # Paginação
+    list_per_page = 10
+
+
+admin.site.register(Receita, ListandoReceitas)
