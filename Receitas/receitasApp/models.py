34041581ce76f569bb0cc530f4fcc185401fb230
Mathvivas <mathvivas@hotmail.com>
2021-03-31 12:30:36 -0300

    Adição de Imagens

--- Receitas/receitasApp/models.py
+++ Receitas/receitasApp/models.py
@@ -12,6 +12,7 @@ class Receita(models.Model):        # Subclasse
     rendimento = models.CharField(max_length=100)
     categoria = models.CharField(max_length=100)
     data_receitas = models.DateTimeField(default=datetime.now, blank=True)
+    foto_receita = models.ImageField(upload_to='fotos/%d/%m/%Y/', blank=True)
     publicada = models.BooleanField(default=False)
 
     # Terminal, mesma pasta de manage.py:
