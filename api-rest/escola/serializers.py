9521340da6fda0494c69075ae8cf320e0856428d
Mathvivas <mathvivas@hotmail.com>
2025-08-14 16:00:46 -0300

    Adicionando Serializers de listar matrículas

--- api-rest/escola/serializers.py
+++ api-rest/escola/serializers.py
@@ -16,3 +16,23 @@ class MatriculaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Matricula
         exclude = [] # Outro modo de selecionar todos
+
+# Listar matrículas por estudante
+class ListaMatriculasEstudanteSerializer(serializers.ModelSerializer):
+    curso = serializers.ReadOnlyField(source='curso.descricao')
+    periodo = serializers.SerializerMethodField()
+    class Meta:
+        model = Matricula
+        fields = ['curso', 'periodo']
+
+    # get_<mesmo campo da variável do SerializerMethodField()>
+    def get_periodo(self, obj):
+        # get_<field name>_display para pegar valores de 
+        # variáveis com dados pré definidos
+        return obj.get_periodo_display()
+    
+class ListaMatriculasCursoSerializer(serializers.ModelSerializer):
+    estudante_nome = serializers.ReadOnlyField(source='estudante.nome')
+    class Meta:
+        model = Matricula
+        fields = ['estudante_nome']
