278e0d68f169f7830b549538f865daf28a54522b
Mathvivas <mathvivas@hotmail.com>
2025-08-15 10:27:56 -0300

    Validação do serializer

--- api-rest/escola/serializers.py
+++ api-rest/escola/serializers.py
@@ -6,6 +6,21 @@ class EstudanteSerializer(serializers.ModelSerializer):
         model = Estudante
         fields = ['id', 'nome', 'email', 'cpf', 'data_nascimento', 'celular']
 
+    def validate_cpf(self, cpf):
+        if len(cpf) != 11:
+            raise serializers.ValidationError('O CPF deve conter 11 dígitos!')
+        return cpf
+
+    def validate_nome(self, nome):
+        if not nome.isalpha():
+            raise serializers.ValidationError('O nome só pode conter letras!')
+        return nome
+    
+    def validate_celular(self, celular):
+        if len(celular) != 13:
+            raise serializers.ValidationError('O celular deve conter 13 dígitos!')
+        return celular
+
 
 class CursoSerializer(serializers.ModelSerializer):
     class Meta:
