4385da70475266a8178179b231d3aa4609dfd1e6
Mathvivas <mathvivas@hotmail.com>
2025-08-14 22:14:16 -0300

    Outra forma de autenticação

--- api-rest/escola/views.py
+++ api-rest/escola/views.py
@@ -2,35 +2,31 @@ from escola.models import Estudante, Curso, Matricula
 from escola.serializers import EstudanteSerializer, CursoSerializer, MatriculaSerializer
 from escola.serializers import ListaMatriculasCursoSerializer, ListaMatriculasEstudanteSerializer
 from rest_framework import viewsets, generics
-from rest_framework.authentication import BasicAuthentication
-from rest_framework.permissions import IsAuthenticated
+# from rest_framework.authentication import BasicAuthentication
+# from rest_framework.permissions import IsAuthenticated, IsAdminUser
+# from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 # viewsets são usados para implementar CRUD
 class EstudanteViewSet(viewsets.ModelViewSet):
-    authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticated]
+    # authentication_classes = [BasicAuthentication]
+    # permission_classes = [IsAdminUser]
 
     queryset = Estudante.objects.all()
     serializer_class = EstudanteSerializer
 
 class CursoViewSet(viewsets.ModelViewSet):
-    authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticated]
+    # authentication_classes = [BasicAuthentication]
+    # permission_classes = [IsAuthenticatedOrReadOnly]
     
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
 
 class MatriculaViewSet(viewsets.ModelViewSet):
-    authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticated]
-    
     queryset = Matricula.objects.all()
     serializer_class = MatriculaSerializer
 
 # generics são usados para implementar visualizações
 class ListaMatriculaEstudante(generics.ListAPIView):
-    authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticated]
     def get_queryset(self):
         # self.kwargs pega todas as infos que são passadas na url
         queryset = Matricula.objects.filter(estudante_id=self.kwargs['pk'])
@@ -38,8 +34,6 @@ class ListaMatriculaEstudante(generics.ListAPIView):
     serializer_class = ListaMatriculasEstudanteSerializer
 
 class ListaMatriculaCurso(generics.ListAPIView):
-    authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticated]
     def get_queryset(self):
         # self.kwargs pega todas as infos que são passadas na url
         queryset = Matricula.objects.filter(curso_id=self.kwargs['pk'])
